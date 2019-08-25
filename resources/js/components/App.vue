d26e3fa66f5283eab46e04630bccadbe70b9392a
Andrew Mackay <mackay-andrew@hotmail.com>
2019-08-25 08:39:51 +0100

    Store jwt in local storage and conditionally hide new post nav tab

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <div>
+      isLoggedIn? {{isLoggedIn}}
+    </div>
     <router-link tag="li" to="/">Home</router-link>
     <router-link tag="li" to="/about">About</router-link>
-    <router-link tag="li" to="/post/new">New Post</router-link>
+    <router-link v-if="isLoggedIn" tag="li" to="/post/new">New Post</router-link>
     <router-view></router-view>
   </div>
 
 </template>
 <script>
 export default {
+  data() {
+    return {
+      // todo add check for is expired
+      isLoggedIn: localStorage.getItem('user-token') != null
+    }
+  }
 }
 </script>
--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -19,6 +19,14 @@ export default {
     async login() {
       try {
         let response = await Authentication.login(this.username, this.password);
+        if(response.status === 200) {
+          const token = response.data.access_token;
+          localStorage.setItem('user-token', token);
+          window.location.replace('/post/new');
+          // todo store when jwt will expire
+          // response.data.expires_in
+        }
+        console.log(response);
       } catch(error){
         console.log("Error:", error)
       }
