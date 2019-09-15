21eae330f9d749602f5078c00d198f8ad7a1c659
Andrew Mackay <mackay-andrew@hotmail.com>
2019-09-15 18:40:36 +0100

    Add and configure image uploader

--- resources/js/components/CreatePost.vue
+++ resources/js/components/CreatePost.vue
@@ -1,42 +1,69 @@
 <template>
-<div id="editor">
-  <input v-model="title" placeholder="title"> <br/>
-  <textarea v-model="summary" placeholder="summary"></textarea><br/>
-  <textarea v-model="content" placeholder="blog post"></textarea> <br/>
-  <button @click="createPost">Save</button>
-  <button @click="showPreview = !showPreview" v-text="showPreview ? 'Hide Preview': 'Show Preview'">Preview</button>
-  <hr v-if=showPreview />
-  <post v-if=showPreview :previewMode="true" :previewPost="previewPostObject"></post>
-</div>
+  <div id="editor">
+    <input v-model="title" placeholder="title" />
+    <br />
+    <textarea v-model="summary" placeholder="summary"></textarea>
+    <br />
+    <textarea v-model="content" placeholder="blog post"></textarea>
+    <br />
+
+    <vue-upload-multiple-image
+      @upload-success="uploadImageSuccess"
+      @before-remove="beforeRemove"
+      @edit-image="editImage"
+      @mark-is-primary="markIsHeader"
+      :data-images="images"
+      :dragText="'Drag and drop images here to upload'"
+      :browseText="'or click browse'"
+      :primaryText="'header image'"
+      :markIsPrimaryText="'use as header image'"
+      :popupText="'This image is set as your header image and will be displayed at the top of your blog post'"
+      :dropText="'Drop image here to upload ...'"
+      :accept="IMAGE_TYPES_ACCEPTED"
+      :maxImage="MAX_NUMBER_OF_IMAGES"
+    ></vue-upload-multiple-image>
+
+    <button @click="createPost">Save</button>
+    <button
+      @click="showPreview = !showPreview"
+      v-text="showPreview ? 'Hide Preview': 'Show Preview'"
+    >Preview</button>
+    <hr v-if="showPreview" />
+    <post v-if="showPreview" :previewMode="true" :previewPost="previewPostObject"></post>
+  </div>
 </template>
 <script>
-import BlogPost from '@/js/services/BlogPost.service.js';
-import marked from 'marked';
-import Post from '@/js/components/Post';
+import BlogPost from "@/js/services/BlogPost.service.js";
+import marked from "marked";
+import Post from "@/js/components/Post";
+import VueUploadMultipleImage from "vue-upload-multiple-image";
 
 export default {
-  components: {Post},
+  components: { Post, VueUploadMultipleImage },
   data() {
     return {
       title: "",
       headerImage: "",
       summary: "",
       content: "",
-
-      showPreview: false
-    }
+      showPreview: false,
+      images: [],
+      IMAGE_TYPES_ACCEPTED:
+        "image/gif,image/jpeg,image/png,image/bmp,image/jpg",
+      MAX_NUMBER_OF_IMAGES: 30
+    };
   },
   computed: {
     previewPostObject() {
       return {
-        "title": this.title,
-        "headerImage": this.headerImage,
-        "summary": this.summary,
-        "content": this.compiledContent
-      }
+        title: this.title,
+        headerImage: this.headerImage,
+        summary: this.summary,
+        content: this.compiledContent
+      };
     },
     compiledContent() {
-      return marked(this.content)
+      return marked(this.content);
     }
   },
   methods: {
@@ -45,18 +72,22 @@ export default {
         title: this.title,
         summary: this.summary,
         content: this.compiledContent
-      }
+      };
       try {
         let response = await BlogPost.createPost(post);
-        if(response.status === 200) {
-          this.$router.push({name: 'post', params: {id: response.data.id}});
+        if (response.status === 200) {
+          this.$router.push({ name: "post", params: { id: response.data.id } });
         }
-      }catch(error){
-        console.log("Error:", error)
+      } catch (error) {
+        console.log("Error:", error);
       }
-    }
+    },
+    uploadImageSuccess() {},
+    beforeRemove() {},
+    editImage() {},
+    markIsHeader() {}
   }
-}
+};
 </script>
 <style scoped>
 /* html, body, #editor {
